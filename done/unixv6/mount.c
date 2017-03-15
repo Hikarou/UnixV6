1da9cd093f64edba3460627281a2f98b6540c56b
José Ferro Pinto <jose.ferropinto@epfl.ch>
2017-03-15 12:49:33 +0100

    Première partie de mountv6 faite, comprendre void* data

---
+++ done/unixv6/mount.c
@@ -0,0 +1,52 @@
+#pragma once
+
+/**
+ * @file mount.h
+ * @brief accessing the UNIX v6 filesystem -- core of the first set of assignments
+ *
+ * @author José Ferro Pinto
+ * @author Marc Favrod
+ * @date mars 2017
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "unixv6fs.h"
+#include "bmblock.h"
+#include "error.h"
+
+/**
+ * @brief  mount a unix v6 filesystem
+ * @param filename name of the unixv6 filesystem on the underlying disk (IN)
+ * @param u the filesystem (OUT)
+ * @return 0 on success; <0 on error
+ */
+int mountv6(const char *filename, struct unix_filesystem *u) {
+   M_REQUIRE_NON_NULL(filename);
+   M_REQUIRE_NON_NULL(unix_filesystem);
+   memset(u, 0, sizeof(&u));
+   u -> fbm = NULL;
+   u -> ibm = NULL;
+   u -> f = fopen(filename, "rd");
+   if (u -> f == NULL) {
+      return ERR_IO;
+   }
+   void* data;
+   int returnSecRead = sector_read(u -> f, 0, data);
+   if (returnSecRead != 0) {
+      return returnSecRead;
+   }
+}
+
+/**
+ * @brief print to stdout the content of the superblock
+ * @param u - the mounted filesytem
+ */
+void mountv6_print_superblock(const struct unix_filesystem *u);
+
+/**
+ * @brief umount the given filesystem
+ * @param u - the mounted filesytem
+ * @return 0 on success; <0 on error
+ */
+int umountv6(struct unix_filesystem *u);
