1f59a479fa22a7e9ad4188202667b64c4bd67bd8
José Ferro Pinto <jose.ferropinto@epfl.ch>
2017-03-15 11:45:11 +0100

    sector_read in sector.c done

---
+++ done/unixv6/sector.c
@@ -0,0 +1,29 @@
+#pragma once
+
+/**
+ * @file  sector.c
+ * @brief block-level accessor function.
+ *
+ * @author José Ferro Pinto
+ * @author Marc Favrod
+ * @date march 2017
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include "error.h"
+/**
+ * @brief read one 512-byte sector from the virtual disk
+ * @param f open file of the virtual disk
+ * @param sector the location (in sector units, not bytes) within the virtual disk
+ * @param data a pointer to 512-bytes of memory (OUT)
+ * @return 0 on success; <0 on error
+ */
+int sector_read(FILE *f, uint32_t sector, void *data) {
+   M_REQUIRE_NON_NULL(f);
+   M_REQUIRE_NON_NULL(data);
+   if (fsetpos(f, sector*SECTOR_SIZE) == -1) {
+      return ERR_IO;
+   }
+   fread(data, SECTOR_SIZE, 1, f);
+}
