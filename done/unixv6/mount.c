804c5cc4dc79b55eb1154059b54223a4b96d0cb1
Marc Favrod-Coune <marcabramfavrodcoune@gmail.com>
2017-03-16 00:06:57 +0100

    Fonction mount terminée et fonctionnelle. Makefile mis à jour

--- done/unixv6/mount.c
+++ done/unixv6/mount.c
@@ -5,7 +5,7 @@
  * @brief accessing the UNIX v6 filesystem -- core of the first set of assignments
  *
  * @author José Ferro Pinto
- * @author Marc Favrod
+ * @author Marc Favrod-Coune
  * @date mars 2017
  */
 
@@ -17,6 +17,7 @@
 #include "error.h"
 #include "mount.h"
 #include "sector.h"
+#include <inttypes.h>
 
 /**
  * @brief  mount a unix v6 filesystem
@@ -27,32 +28,51 @@
 int mountv6(const char *filename, struct unix_filesystem *u) {
    M_REQUIRE_NON_NULL(filename);
    M_REQUIRE_NON_NULL(u);
-   memset(u, 0, sizeof(&u));
+   memset(u, 0, sizeof(*u)); // ici tu avais mis & mais le prof a mis *: * me parait plus juste
    u -> fbm = NULL;
    u -> ibm = NULL;
    u -> f = fopen(filename, "rd");
    if (u -> f == NULL) {
       return ERR_IO;
    }
-   void* data = NULL;
+   
+   uint8_t data[SECTOR_SIZE];
    int returnSecRead = ERR_IO;
-   returnSecRead = sector_read(u -> f, BOOTBLOCK_SECTOR, data);
+   returnSecRead = sector_read(u -> f, BOOTBLOCK_SECTOR, data); 
+
    if (returnSecRead != 0) {
       return returnSecRead;
    }
-   fseek(data,BOOTBLOCK_MAGIC_NUM_OFFSET,0);
-   uint8_t toCheck;
-   fread(&toCheck, sizeof(BOOTBLOCK_MAGIC_NUM), 1, data);
+
+	uint8_t toCheck;
+	toCheck = data[BOOTBLOCK_MAGIC_NUM_OFFSET];
+   
+   
    if (toCheck !=BOOTBLOCK_MAGIC_NUM) {
       return ERR_BADBOOTSECTOR;
    }
-   void* superblock=NULL;
+   uint8_t superblock[SECTOR_SIZE];
    returnSecRead = sector_read(u -> f, SUPERBLOCK_SECTOR, superblock);
    if (returnSecRead != 0) {
       return returnSecRead;
    }
-   fseek(superblock, 0, 0);
-   fread(&(u -> s), SECTOR_SIZE, 1, superblock);
+   
+   u -> s.s_isize = (superblock[1] << 8) + superblock[0];
+   u -> s.s_fsize = (superblock[3] << 8) + superblock[2];
+   u -> s.s_fbmsize = (superblock[5] << 8) + superblock[4]; 
+   u -> s.s_ibmsize = (superblock[7] << 8) + superblock[6]; 
+   u -> s.s_inode_start = (superblock[9] << 8) + superblock[8]; 
+   u -> s.s_block_start = (superblock[11] << 8) + superblock[10]; 
+   u -> s.s_fbm_start = (superblock[13] << 8) + superblock[12]; 
+   u -> s.s_ibm_start = (superblock[15] << 8) + superblock[14];
+   u -> s.s_flock = superblock[16];
+   u -> s.s_ilock = superblock[17];
+   u -> s.s_fmod = superblock[18];
+   u -> s.s_ronly = superblock[19];
+   u -> s.s_time[0] = (superblock[21] << 8) + superblock[20];
+   u -> s.s_time[1] = (superblock[23] << 8) + superblock[22];
+   
+   
    return 0;
 }
 
@@ -60,11 +80,43 @@ int mountv6(const char *filename, struct unix_filesystem *u) {
  * @brief print to stdout the content of the superblock
  * @param u - the mounted filesytem
  */
-void mountv6_print_superblock(const struct unix_filesystem *u);
+void mountv6_print_superblock(const struct unix_filesystem *u)
+{
 
 /**
  * @brief umount the given filesystem
  * @param u - the mounted filesytem
  * @return 0 on success; <0 on error
  */
-int umountv6(struct unix_filesystem *u);
+ 	 
+	 FILE* output = stdout;
+	 
+	 fprintf(output, "*********FS SUPERBLOCK START**********\n");
+	 fprintf(output, "s_isize : %" PRIu16 "\n", u -> s.s_isize); 
+	 fprintf(output, "s_fsize : %" PRIu16 "\n", u -> s.s_fsize);
+	 fprintf(output, "s_fbmsize : %" PRIu16 "\n", u -> s.s_fbmsize); 
+	 fprintf(output, "s_ibmsize : %" PRIu16 "\n", u -> s.s_ibmsize); 
+	 fprintf(output, "s_inode_start : %" PRIu16 "\n", u -> s.s_inode_start); 
+	 fprintf(output, "s_block_start : %" PRIu16 "\n", u -> s.s_block_start); 
+	 fprintf(output, "s_fbm_start : %" PRIu16 "\n", u -> s.s_fbm_start); 
+	 fprintf(output, "s_ibm_start : %" PRIu16 "\n", u -> s.s_ibm_start); 
+	 fprintf(output, "s_flock : %" PRIu8 "\n", u -> s.s_flock);
+	 fprintf(output, "s_ilock : %" PRIu8 "\n", u -> s.s_ilock);
+	 fprintf(output, "s_fmod : %" PRIu8 "\n", u -> s.s_fmod);
+	 fprintf(output, "s_ronly : %" PRIu8 "\n", u -> s.s_ronly);
+	 fprintf(output, "s_time : [%" PRIu16 "] %" PRIu16 "\n", u -> s.s_time[0], u -> s.s_time[1]); 
+	 /* pour la dernière ligne, je ne suis pas sûr s'il veut qu'on affiche entre [] la taille du
+	  tableau ou simplement les deux cases du tableau (dont la première entre crochet)*/ 
+	 fprintf(output, "*********FS SUPERBLOCK END**********\n");
+	 
+ }
+int umountv6(struct unix_filesystem *u)
+{	
+	M_REQUIRE_NON_NULL(u);
+	
+	if(!fclose(u -> f)){
+		return ERR_IO;
+	}	
+	
+	return 0; 
+}
--- done/unixv6/sector.c
+++ done/unixv6/sector.c
@@ -5,7 +5,7 @@
  * @brief block-level accessor function.
  *
  * @author José Ferro Pinto
- * @author Marc Favrod
+ * @author Marc Favrod-Coune
  * @date march 2017
  */
 
@@ -23,9 +23,13 @@
  */
 int sector_read(FILE *f, uint32_t sector, void *data) {
    M_REQUIRE_NON_NULL(f);
-   if (fseek(f, sector * SECTOR_SIZE, 0) == -1) {
+   size_t read = 0;
+   if (fseek(f, sector * SECTOR_SIZE, SEEK_SET) == -1) {
       return ERR_IO;
    }
-   fread(data, SECTOR_SIZE, 1, f);
+   read  = fread(data, SECTOR_SIZE, 1, f);
+   if (read == 0){
+   		return ERR_IO;
+   }
    return 0;
 }
