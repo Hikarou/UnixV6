54a0ceff6ba3e10a6cc9b897f8dc5072070725a4
José Ferro Pinto <jose.ferropinto@epfl.ch>
2017-03-15 11:54:59 +0100

    Makefile updated and sector_read corrected

--- done/unixv6/sector.c
+++ done/unixv6/sector.c
@@ -12,6 +12,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "error.h"
+#include "unixv6fs.h"
+
 /**
  * @brief read one 512-byte sector from the virtual disk
  * @param f open file of the virtual disk
@@ -22,7 +24,7 @@
 int sector_read(FILE *f, uint32_t sector, void *data) {
    M_REQUIRE_NON_NULL(f);
    M_REQUIRE_NON_NULL(data);
-   if (fsetpos(f, sector*SECTOR_SIZE) == -1) {
+   if (fseek(f, sector * SECTOR_SIZE, 0) == -1) {
       return ERR_IO;
    }
    fread(data, SECTOR_SIZE, 1, f);
