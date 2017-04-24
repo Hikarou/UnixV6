9804fbe3193c83840beb80d7342afa247a981bf2
José Ferro Pinto <jose.ferropinto@epfl.ch>
2017-04-24 11:24:04 +0200

    Suppression de l'inutile fait

--- done/fs.c
+++ done/fs.c
@@ -15,23 +15,11 @@
 #include <errno.h>
 #include <fcntl.h>
 
-static const char *fs_str = "Hello World!\n";
-static const char *fs_path = "/hello";
-
 static int fs_getattr(const char *path, struct stat *stbuf)
 {
     int res = 0;
 
     memset(stbuf, 0, sizeof(struct stat));
-    if (strcmp(path, "/") == 0) {
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
-    } else if (strcmp(path, fs_path) == 0) {
-        stbuf->st_mode = S_IFREG | 0444;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = strlen(fs_str);
-    } else
-        res = -ENOENT;
 
     return res;
 }
@@ -42,12 +30,8 @@ static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     (void) offset;
     (void) fi;
 
-    if (strcmp(path, "/") != 0)
-        return -ENOENT;
-
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
-    filler(buf, fs_path + 1, NULL, 0);
 
     return 0;
 }
@@ -55,20 +39,7 @@ static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 static int fs_read(const char *path, char *buf, size_t size, off_t offset,
                       struct fuse_file_info *fi)
 {
-    size_t len;
     (void) fi;
-    if(strcmp(path, fs_path) != 0)
-        return -ENOENT;
-
-    len = strlen(fs_str);
-    if (offset < len) {
-        if (offset + size > len)
-            size = len - offset;
-        memcpy(buf, fs_str + offset, size);
-    } else
-        size = 0;
-
-    return size;
 }
 
 static struct fuse_operations available_ops = {
