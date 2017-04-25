4d6a450fe794314020f8372dfc586eeb1398df78
José Ferro Pinto <jose.ferropinto@epfl.ch>
2017-04-25 16:59:59 +0200

    fs_getattr almost done

--- done/fs.c
+++ done/fs.c
@@ -8,18 +8,61 @@
 */
 
 #define FUSE_USE_VERSION 26
-
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include "mount.h"
+#include "sector.h"
+#include "direntv6.h"
+#include "error.h"
+#include "inode.h"
+
+struct unix_filesystem fs;
 
 static int fs_getattr(const char *path, struct stat *stbuf)
 {
     int res = 0;
 
+    //Je ne suis pas sûr si cette vérification est obligatoire
+    if (fs.f == NULL) {
+        exit(1);
+    }
+
+    int inode_nb = direntv6_dirlookup(&fs, ROOT_INUMBER, path);
+    if (inode_nb < 0) {
+	return inode_nb;
+    }
+
+    struct inode i;
+    int err = inode_read(&fs, inode_nb, &i);
+    if (err != 0) {
+        return err;
+    }
+
+    //For debug purpose
+    inode_print(&i);
+
     memset(stbuf, 0, sizeof(struct stat));
+
+    
+    //stbuf -> st_dev = ????; //Je ne sais pas quoi mettre là dedans
+    stbuf -> st_ino = inode_nb;
+    stbuf -> st_mode = (i.i_mode & IFDIR) ? S_IFDIR : S_IFREG;
+    //Je ne suis pas sûr que l'on puisse séparer ça en deux, mais c'est plus joli, je testerai plus tard
+    stbuf -> st_mode = stbuf -> st_mode | S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
+    stbuf -> st_nlink = i.i_nlink;
+    stbuf -> st_uid = i.i_uid;
+    stbuf -> st_gid = i.i_gid;
+    //stbuf -> st_rdev = ????;
+    stbuf -> st_size = inode_getsize(&i);
+    //stbuf -> st_blksize = ????;
+    //stbuf -> st_blocks = ????;
+    //stbuf -> st_atim = i.atime; //ces deux lignes plantent car elle demandent des types timespec
+    //stbuf -> st_mtim = i.mtime;
+    //stbuf -> st_ctim = ????;
 
     return res;
 }
@@ -48,7 +91,33 @@ static struct fuse_operations available_ops = {
     .read	= fs_read,
 };
 
+/* From https://github.com/libfuse/libfuse/wiki/Option-Parsing.
+ * This will look up into the args to search for the name of the FS.
+ */
+static int arg_parse(void *data, const char *filename, int key, struct fuse_args *outargs)
+{
+    (void) data;
+    (void) outargs;
+    if (key == FUSE_OPT_KEY_NONOPT && fs.f == NULL && filename != NULL) {
+
+	int err = mountv6(filename, &fs);
+
+	if (err != 0) {
+           puts(ERR_MESSAGES[err - ERR_FIRST]);
+	   exit(1);
+	}
+        return 0;
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
-    return fuse_main(argc, argv, &available_ops, NULL);
+    struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+    int ret = fuse_opt_parse(&args, NULL, NULL, arg_parse);
+    if (ret == 0) {
+        ret = fuse_main(args.argc, args.argv, &available_ops, NULL);
+        (void)umountv6(&fs);
+    }
+    return ret;
 }
