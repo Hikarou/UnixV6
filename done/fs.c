8313a1be80d1308f1611795b2fa2cdc67c42deef
Marc Favrod-Coune <marcabramfavrodcoune@gmail.com>
2017-04-25 19:21:55 +0200

    modifications

--- done/fs.c
+++ done/fs.c
@@ -27,13 +27,14 @@ static int fs_getattr(const char *path, struct stat *stbuf)
     int res = 0;
 
     //Je ne suis pas sûr si cette vérification est obligatoire
+    // OUI je pense que c'est bien
     if (fs.f == NULL) {
         exit(1);
     }
 
     int inode_nb = direntv6_dirlookup(&fs, ROOT_INUMBER, path);
     if (inode_nb < 0) {
-	return inode_nb;
+		return inode_nb;
     }
 
     struct inode i;
@@ -48,7 +49,7 @@ static int fs_getattr(const char *path, struct stat *stbuf)
     memset(stbuf, 0, sizeof(struct stat));
 
     
-    //stbuf -> st_dev = ????; //Je ne sais pas quoi mettre là dedans
+    //stbuf -> st_dev = ????; //Je ne sais pas quoi mettre là dedans:
     stbuf -> st_ino = inode_nb;
     stbuf -> st_mode = (i.i_mode & IFDIR) ? S_IFDIR : S_IFREG;
     //Je ne suis pas sûr que l'on puisse séparer ça en deux, mais c'est plus joli, je testerai plus tard
@@ -56,10 +57,11 @@ static int fs_getattr(const char *path, struct stat *stbuf)
     stbuf -> st_nlink = i.i_nlink;
     stbuf -> st_uid = i.i_uid;
     stbuf -> st_gid = i.i_gid;
-    //stbuf -> st_rdev = ????;
+    //stbuf -> st_rdev = ????; 
     stbuf -> st_size = inode_getsize(&i);
-    //stbuf -> st_blksize = ????;
-    //stbuf -> st_blocks = ????;
+    //stbuf -> st_blksize = ????;   The st_blksize field gives the "preferred" blocksize for efficient filesystem I/O.   (Writing to a file in smaller chunks may cause an inefficient read-modify-rewrite.)
+
+    stbuf -> st_blocks = (stbuf -> st_size)/SECTOR_SIZE;
     //stbuf -> st_atim = i.atime; //ces deux lignes plantent car elle demandent des types timespec
     //stbuf -> st_mtim = i.mtime;
     //stbuf -> st_ctim = ????;
@@ -70,11 +72,38 @@ static int fs_getattr(const char *path, struct stat *stbuf)
 static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
                          off_t offset, struct fuse_file_info *fi)
 {
+	int err = 0;
+	
     (void) offset;
     (void) fi;
 
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
+    
+    int inode_nb = direntv6_dirlookup(&fs, ROOT_INUMBER, path);
+    if (inode_nb < 0) {
+		return inode_nb;
+    }
+    
+    struct inode i;
+    int err = inode_read(&fs, inode_nb, &i);
+    if (err != 0) {
+        return err;
+    }
+    
+    if (!(i.i_mode & IFDIR)){
+    	printf("Problem: required inode on directory\n);
+    	return ERR_BAD_PARAMETER;
+    } 
+
+	struct directory_reader d; 
+	err = direntv6_opendir(&fs, inode_nb, &d);
+	
+	while (err == 1) {
+		// loop sur d -> dirs pour les afficher tous.
+		filler(buf, 
+		err = direntv6_opendir(&fs, inode_nb, &d);
+	}
 
     return 0;
 }
@@ -100,12 +129,12 @@ static int arg_parse(void *data, const char *filename, int key, struct fuse_args
     (void) outargs;
     if (key == FUSE_OPT_KEY_NONOPT && fs.f == NULL && filename != NULL) {
 
-	int err = mountv6(filename, &fs);
+		int err = mountv6(filename, &fs);
 
-	if (err != 0) {
-           puts(ERR_MESSAGES[err - ERR_FIRST]);
-	   exit(1);
-	}
+		if (err != 0) {
+		       puts(ERR_MESSAGES[err - ERR_FIRST]);
+		   exit(1);
+		}
         return 0;
     }
     return 1;
